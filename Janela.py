a0290fc4ab101be6ad9670e5a0156168f63eb424
J0aoMath <joaomath.develop@gmail.com>
2024-03-22 20:35:33 -0300

    adicionando interação entre os programas

--- Janela.py
+++ Janela.py
@@ -1,4 +1,5 @@
-from tkinter import *
+from tkinter import * 
+#from contas import *
 
 #Configurações Menu Inicial
 m_i = Tk()
@@ -9,16 +10,16 @@ comp_M = 750
 alt_M = 450
 inf1 = comp_T/2 - comp_M/2
 inf2 = alt_T/2 - alt_M/2 - 50
-#m_i.geometry('%dx%d+%d+%d'%(comp_M, alt_M, inf1, inf2))
+m_i.geometry('%dx%d+%d+%d'%(comp_M, alt_M, inf1, inf2))
 
 #Frames e Labels
 #label_1 = Label(m_i, text='Escolha seu exercício:', font='bahscript 24', bd=3)
 #label_1.grid(row=0, column=0)
 #Botões
-btn_soma_i=PhotoImage(file='Imagens\Mais.png')
-btn_subtracao_i=PhotoImage(file='Imagens\Menos.png')
-btn_multiplicacao_i=PhotoImage(file='Imagens\Vezes.png')
-btn_divisao_i=PhotoImage(file='Imagens\Dividido.png')
+btn_soma_i=PhotoImage(file="Imagens\Mais.png")
+btn_subtracao_i=PhotoImage(file="Imagens\Menos.png")
+btn_multiplicacao_i=PhotoImage(file="Imagens\Vezes.png")
+btn_divisao_i=PhotoImage(file="Imagens\Dividido.png")
 btn_soma = Button(m_i, image=btn_soma_i)
 btn_soma.grid(row=1,column=0)
 btn_subtracao = Button(m_i, image=btn_subtracao_i)
---
+++ contas.py
@@ -0,0 +1,68 @@
+from random import randint
+#Linha
+
+def linha():
+    print('=-'*13)
+class calculos:
+    #Exercicios de subtração    
+    def subtracao():
+        while True:
+            a = randint(1,99)
+            b = randint(1,99)
+            res = a-b
+            if res > 0:
+                break
+        res2 = int(input(f'Quanto é {a}-{b}? '))
+        if res == res2:
+            print('Correto!')
+        else:
+            print('Incorreto :(')
+        linha()
+    #Exercícios de soma
+    def soma():
+        a = randint(1,99)
+        b = randint(1,99)
+        res = a+b
+        res2 = int(input(f'Quanto é {a}+{b}? '))
+        if res2 == res:
+            print('Correto!')
+        else:
+            print('Incorreto :(')
+        linha()
+    #Exercícios de divisão
+    def divisao():
+        while True:
+            a = randint(1,99)
+            b = randint(1,9)
+            res = a/b
+            if res in range(1,99):
+                break
+        res2 = int(input(f'Quanto é {a}/{b}? '))
+        if res == res2:
+            print('Correto!')
+        else:
+            print('Incorreto :(')
+        linha()
+    #Exercícios de multiplicação
+    def multiplicacao():
+        a = randint(1,9)
+        b = randint(1,9)
+        res = a*b
+        res2 = int(input(f'Quanto é {a}x{b}? '))
+        if res == res2:
+            print('Correto!')
+        else:
+            print('Incorreto :(')
+        linha()
+    #Código
+    while True:
+        subtracao()
+        soma()
+        divisao()
+        multiplicacao()
+
+        aba = input('Quer continuar? [S/N] : ')
+        if aba in 'Nn':
+            print('Ok, tenha um bom dia!')
+            break
+        linha()
